73472d3180423797a3f83f677fb89195b1ecffb0
kamen <hello@kamen.cn>
2025-04-04 13:52:46 +0800

    工作台业务代码

--- sky-server/src/main/java/com/sky/mapper/SetMealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetMealMapper.java
@@ -14,6 +14,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 
 @Mapper
@@ -82,4 +83,11 @@ public interface SetMealMapper {
             "from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
             "where sd.setmeal_id = #{setmealId}")
     List<DishItemVO> getDishItemBySetmealId(Long setmealId);
+
+    /**
+     * 根据条件统计套餐数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
